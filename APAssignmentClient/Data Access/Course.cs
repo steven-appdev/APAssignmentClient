f8359e779b601825f0b0196083480c28e0c2eb97
Steven Tan <steventan@outlook.my>
2023-04-01 17:56:14 +0100

    Added Data Access

---
+++ APAssignmentClient/Data Access/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APAssignmentClient.Data_Access
+{
+    public class Course
+    {
+        [Key]
+        public int courseId { get; set; }
+
+        [Required]
+        public String courseName { get; set; }
+
+        [Required]
+        public String courseDescription { get; set; }
+
+        [DefaultValue(0.00)]
+        public double coursePrice { get; set; }
+    }
+}
