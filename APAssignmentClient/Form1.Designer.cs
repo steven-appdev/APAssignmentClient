972127aa063acf570f8ecb9512c4ba75cbbeb5ce
Steven Tan <steventan@outlook.my>
2023-04-01 15:17:17 +0100

    Added UI

--- APAssignmentClient/Form1.Designer.cs
+++ APAssignmentClient/Form1.Designer.cs
@@ -28,13 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lsbEnrolledCourses = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome, (username)";
+            // 
+            // lsbEnrolledCourses
+            // 
+            this.lsbEnrolledCourses.FormattingEnabled = true;
+            this.lsbEnrolledCourses.Location = new System.Drawing.Point(12, 122);
+            this.lsbEnrolledCourses.Name = "lsbEnrolledCourses";
+            this.lsbEnrolledCourses.Size = new System.Drawing.Size(776, 303);
+            this.lsbEnrolledCourses.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(204, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enrolled Courses";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(656, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "+ Enrol New Course";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lsbEnrolledCourses);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Dashboard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lsbEnrolledCourses;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
