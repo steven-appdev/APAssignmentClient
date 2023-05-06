b784d987cf9d2b11dfaf466b3e9e27151ae02bf4
Steven Tan <steventan@outlook.my>
2023-05-06 20:20:10 +0100

    Removed development only code

--- APAssignmentClient/Program.cs
+++ APAssignmentClient/Program.cs
@@ -19,25 +19,6 @@ namespace APAssignmentClient
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            ///Running Client side
-            //ClientDashboard screen = new ClientDashboard();
-            //ClientModel clientModel = ClientModel.GetInstance();
-            //CourseModel courseModel = CourseModel.GetInstance();
-            //BookingModel bookingModel = BookingModel.GetInstance();
-
-            ////Simulate user has login (DEV MODE ONLY) (REMOVE IN FINAL PRODUCT)
-            //IDataAccess data = new DataAccess();
-            //clientModel.SetClient(data.RetrieveClientInformation(1));
-            //ClientDashboardPresenter presenter = new ClientDashboardPresenter(screen, clientModel, courseModel, bookingModel);
-
-            ///Running Admin side
-            //AdminDashboard screen = new AdminDashboard();
-            //CourseModel courseModel = CourseModel.GetInstance();
-            //ClientModel clientModel = ClientModel.GetInstance();
-            //StaffModel staffModel = StaffModel.GetInstance();
-            //AdminDashboardPresenter presenter = new AdminDashboardPresenter(screen, courseModel, clientModel, staffModel);
-
-            ///Running Login client
             Login screen = new Login();
             AccountModel accountModel = AccountModel.GetInstance();
             ClientModel clientModel = ClientModel.GetInstance();
