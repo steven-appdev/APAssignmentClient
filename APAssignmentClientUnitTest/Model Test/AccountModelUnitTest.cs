6fcf0eee64894ea9f8997d31aaefaa608b333703
Steven Tan <steventan@outlook.my>
2023-05-07 12:43:22 +0100

    Added AccountModel Unit Test

---
+++ APAssignmentClientUnitTest/Model Test/AccountModelUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using APAssignmentClient.Model;
+
+namespace APAssignmentClientUnitTest.ModelTest
+{
+    [TestClass]
+    public class AccountModelUnitTest
+    {
+        private IAccountModel accountModel;
+
+        public AccountModelUnitTest()
+        {
+            accountModel = AccountModel.GetInstance();
+        }
+
+        [TestMethod]
+        public void TestMethod53()
+        {
+            accountModel.UserID = 1;
+            accountModel.UserType = 1;
+            Assert.AreEqual(1, accountModel.UserID);
+            Assert.AreEqual(1, accountModel.UserType);
+        }
+
+        [TestMethod]
+        public void TestMethod54()
+        {
+            bool result = accountModel.Login("steven", "abc123");
+            Assert.IsTrue(result);
+        }
+
+    }
+}
